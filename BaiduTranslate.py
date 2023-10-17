1dd14ec1fe3c5a8bdefa158414470318cd6ad945
chflame163 <chflame@163.com>
2023-10-17 21:31:33 +0800

    first commit

---
+++ BaiduTranslate.py
@@ -0,0 +1,150 @@
+import http.client
+import hashlib
+import urllib
+import random
+import json
+import requests
+import execjs
+import os
+
+
+# 读取百度翻译的加盐算法
+js_file_path = os.path.dirname(__file__) + '/js/BaiduTranslate_sign.js'
+with open(js_file_path, 'r', encoding='utf-8') as f:
+    sign_js = execjs.compile(f.read())
+
+# 判断字符串是否包含中文
+def is_contain_chinese(check_str):
+    for ch in check_str:
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+    return False
+
+# 开发者API翻译模块
+class BaiduTrans_devapi:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            'required': {
+                'text': ('STRING', {'multiline': True}),
+                'Translate_to_language': (['en', 'zh'], {'default': 'en'}),
+            },
+        }
+
+    RETURN_TYPES = ('STRING',)
+    FUNCTION = 'translation_devapi'
+    CATEGORY = 'Baidu Translate'
+
+    def translation_devapi(self, Translate_to_language, text):
+
+        # 填写你的BaiduTrans开发账号appid及密钥
+        appid = '000000000000000000' #此处引号内填入appid
+        appkey = 'xxxxxxxxxxxxxxxxxxxx'  #此处引号内填入密钥
+
+        # For list of language codes, please refer to `https://api.fanyi.baidu.com/doc/21`
+        from_lang = 'auto'
+        to_lang = Translate_to_language
+
+        endpoint = 'http://api.fanyi.baidu.com'
+        path = '/api/trans/vip/translate'
+        url = endpoint + path
+        query = text
+        translate_result = ''
+
+        # Generate salt and sign
+        salt = random.randint(32768, 65536)
+        s = appid + query + str(salt) + appkey
+        sign = hashlib.md5(s.encode('utf-8')).hexdigest()
+
+        # Build request
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        payload = {'appid': appid, 'q': query, 'from': from_lang, 'to': to_lang, 'salt': salt, 'sign': sign}
+
+        # Send request
+        try:
+            r = requests.post(url, params=payload, headers=headers)
+            result = r.json()
+            translate_result = result['trans_result'][0]['dst']
+
+        except Exception as e:
+            print(e)
+
+        # Show response
+        # print(json.dumps(result, indent=4, ensure_ascii=False))
+        print('BaiduTrans_Api:' + text + ' ---> ' + translate_result)
+        return (translate_result,)
+
+class BaiduTrans_v2trans:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            'required': {
+                'text': ('STRING', {'multiline': True}),
+                'Translate_to_language': (['en', 'zh'], {'default': 'en'}),
+            },
+        }
+
+    RETURN_TYPES = ('STRING',)
+    FUNCTION = 'translation_v2trans'
+    CATEGORY = 'Baidu Translate'
+    OUTPUT_NODE = True
+
+    def translation_v2trans(self, Translate_to_language, text):
+
+        from_lang = 'auto'
+        to_lang = Translate_to_language
+        translate_result = ''
+
+        token = '012cd082bf1f821bb7d94981bf6d477a'
+        url = 'https://fanyi.baidu.com/v2transapi'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+            'cookie': 'BIDUPSID=3641572D5E0DB57A2F20F8F3373E302C; PSTM=1687090179; '
+                      'BAIDUID=3641572D5E0DB57AF59F1D83EEBC5D2B:FG=1; BAIDUID_BFESS=3641572D5E0DB57AF59F1D83EEBC5D2B:FG=1; '
+                      'ZFY=sGU1ho9nxRf2CX2bcYMVcfSXr9y2:BmKBeBdv7CDGhUs:C; '
+                      'BDUSS'
+                      '=tXaEJQVkxBeVBHMllBWWh1aTVZLXlhcVVqTWNCOXJGfmwzUUJmaHphWm1zZGRrSVFBQUFBJCQAAAAAAAAAAAEAAADWpvEyzqiwrsTjtcTQocPXAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGYksGRmJLBkam; BDUSS_BFESS=tXaEJQVkxBeVBHMllBWWh1aTVZLXlhcVVqTWNCOXJGfmwzUUJmaHphWm1zZGRrSVFBQUFBJCQAAAAAAAAAAAEAAADWpvEyzqiwrsTjtcTQocPXAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGYksGRmJLBkam; newlogin=1; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; BA_HECTOR=00aka5a12g80a10g25a52l0g1ie1gm11p; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; BDRCVFR[feWj1Vr5u3D]=I67x6TjHwwYf0; PSINO=6; delPer=0; H_PS_PSSID=36550_39112_39226_39222_39097_39039_39198_39207_26350_39138_39225_39094_39137_39101; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1692451747; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1692451747; ab_sr=1.0.1_ZmQ3OWYzODRjZGNkOTYxOWI4ZTVhYjRmNTAwNjYyYTUwYmI3OGY2NTViMzhkNWYzM2IxZTVhNjAwNjdkMTU0ODE4Yzc2YmI3OGRmNTY3Y2QxMzZiZDRmZDIwMGIwYmQ2NGI5M2QzZWFlNmNkODBhZjllZDcxNGFkMTEyNmY0NGNhZGZjMTlmOGQ2YjIxNzNhMmUxNDJkMDhlZTM1NjhiZjkyMDc2MmQxN2Q5ODg3NDBkZGViNTEzMDU2NDQzNGEy'}
+
+        sign = sign_js.call('e', text)
+        data = {'from': from_lang,
+                'to': to_lang,
+                'query': text,
+                'transtype': 'realtime',
+                'simple_means_flag': 3,
+                'sign': sign,
+                'domain': 'common',
+                'token': token}
+
+        try:
+            baidutranslate = requests.post(url, headers=headers, data=data).json()
+            translate_result = baidutranslate['trans_result']['data'][0]['dst']
+
+        except Exception as e:
+            print(e)
+
+        # 如果目标语言是英语 且 翻译结果包含中文 且 原始文本不包含中文，返回原文
+        if Translate_to_language == 'en' and is_contain_chinese(translate_result) and not is_contain_chinese(text):
+            translate_result = text
+        # 如果目标语言是中文 且 翻译结果不包含中文 且 原始文本包含中文，返回原文
+        if Translate_to_language == 'zh' and not is_contain_chinese(translate_result) and is_contain_chinese(text):
+            translate_result = text
+
+        print('BaiduTrans_v2trans:' + text + ' ---> ' + translate_result)
+        return (translate_result,)
+
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    'BaiduTrans_DevApi': BaiduTrans_devapi,
+    'BaiduTrans_v2Trans': BaiduTrans_v2trans,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    'BaiduTrans_DevApi': 'BaiduTrans(DevApi)',
+    'BaiduTrans_v2Trans': 'BaiduTrans(v2Trans)',
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ComfyUI_BaiduTranslate
- A Chinese and English text translation plugin for ComfyUI.
+# ComfyUI_BaiduTranslate  
+ComfyUI下使用的中英文文本翻译插件。  
+
+![img](image/BaiduTrans_plugin.png)  
+
+### 插件调整项：  
+**Translate_to_language：** 翻译目标语言。en是英语，zh是简体中文。  
+
+## 节点说明：  
+有两个节点可供使用，区别是使用了不同的翻译接口。  
+### BaiduTrans(use DevApi):  
+使用百度翻译个人开发者apiid及密钥，每月100万字符免费额度。  
+需要在[https://fanyi-api.baidu.com/](https://fanyi-api.baidu.com/) 免费申请成为开发者，并在控制台开通高级版即可使用，你将获得一个APIid和对应的密钥。  
+用文本编辑软件打开BaiduTranslate.py，在44和45行的引号内分别填入你的APIid和密钥，保存文件。重启ComfyUI生效。  
+
+### BaiduTrans(v2trans)：  
+从百度翻译https公共接口用爬虫获取翻译结果。这个接口不太稳定，对于中英文混合文本的翻译支持不好。所以做了一些判断来避免发生返回错误。  
+当目标语言设置为英语时，如果返回内容包含中文，则直接返回原文。当目标语言为中文时，如果返回内容不包含中文，亦直接返回原文。  
+注意，这个节点偶尔出现英译中长句返回空结果的问题，这并不是本插件的问题所导致。  
+
+## 安装方法：  
+- 解压zip文件，将"ComfyUI_BaiduTranslate"文件夹复制到 ComfyUI\custom_nodes\  
+- 安装依赖包，在资源管理器ComfyUI\custom_nodes\ComfyUI_BaiduTranslate\ 这个位置打开cmd窗口，输入以下命令：  
+```..\..\..\python_embeded\python.exe -s -m pip install -r requirements.txt```  
+- 重新打开ComfyUI  
+
+## 使用方法：  
+- 在节点文本框输入中文或英文，下面“Translate_to_language”选择要翻译的目标语言。  
+- 如需预览文本，可后面接一个“Preview Text Node”（需先安装 [ComfyUI_Custom_Nodes_AlekPet](https://github.com/AlekPet/ComfyUI_Custom_Nodes_AlekPet)）  
+
